88f37db502aa05ac0bd671ed1092a3befa4f06cd
danil-nizamov <danil.nizamov+github.codesignal.com>
2025-07-21 23:42:12 +0300

    updated static URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SCENARIO_URL = 'scenarios/example.json';
-const SOUNDS_DIR = 'sounds/';
+const SCENARIO_URL = 'some_tree/scenarios/example.json';
+const SOUNDS_DIR = 'some_tree/sounds/';
 
 const app = document.getElementById('app');
 
